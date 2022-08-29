8492d371feb2b220a9fbe831dbbd7ca5f4520c98
huutuong2411 <phanhuutuong2411@gmail.com>
2022-08-29 21:40:59 +0700

    Bai tap 1

---
+++ helloworld.rb
@@ -0,0 +1,20 @@
+#bài 1:
+puts "Họ Và Tên: Phan Hữu Tường "
+puts "MSV: 1911505310160"
+
+#bài 2: 
+puts "Nhập 1 số:"
+so = gets
+puts "số: #{so}"
+
+#bài 3:
+puts "Tìm số lớn hơn"
+puts "nhập số thứ nhất:"
+so1 = gets
+puts "nhập số thứ hai:"
+so2 = gets
+if so1 > so2
+    puts "số lớn hơn là: #{so1}"
+ else 
+    puts "số lớn hơn là: #{so2}"
+ end
